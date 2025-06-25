92fe3dbedb37f716424e97e8150322256e6d6e05
Adailton Júnior <adailtonjunior0604@gmail.com>
2025-06-25 00:35:36 -0300

    add

---
+++ CourseJSFull/teste/exerciciosIniciais/sistemaMultas.js
@@ -0,0 +1,8 @@
+var vel = 81
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel> 80) {
+    console.log(`Multa por excesso de velocidade`)
+} else {
+    console.log(`Sem multas`)
+}
+console.log(`Dirija com segurança e prudência!`)
