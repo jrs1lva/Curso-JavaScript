8fe03fed55582709f4ecc5d27b257cb98c8af628
Adailton Júnior <adailtonjunior0604@gmail.com>
2025-07-03 16:44:44 -0300

    Tabuada

--- CourseJSFull/meusExercicios/tabuada/script.js
+++ CourseJSFull/meusExercicios/tabuada/script.js
@@ -1,1 +1,11 @@
-alert('Olá')
+function tabuada() {
+    var num = document.getElementById('txtnum')
+    var n1 = Number(num.value)
+    var res = document.getElementById('res')
+
+    for (let i = 1; i <= 10; i++) {
+        let m = n1 * i
+        res.innerHTML += `${n1} x ${i} = ${m}<br>`
+        
+    }
+}
