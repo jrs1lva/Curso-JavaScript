a52705f9868b74dbab842b379de69626eef3b263
Adailton Júnior <adailtonjunior0604@gmail.com>
2025-07-03 22:02:20 -0300

    finalizando tabuada

--- CourseJSFull/meusExercicios/contaPassos/script.js
+++ CourseJSFull/meusExercicios/contaPassos/script.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function contar() {
+    var txtnum = document.getElementById('txtnum')
+    var n1 = Number(txtnum.value)
+    var res = document.querySelector('div#res')
+
+    for (let i = 0; i < n1; i++) {
+        if (i == 0) {
+            res.innerHTML = 'Contando...'
+        } else {
+            res.innerHTML = `Passo ${i}`
+        }
+
+    }
+}
--- CourseJSFull/meusExercicios/tabuada/script.js
+++ CourseJSFull/meusExercicios/tabuada/script.js
@@ -2,10 +2,14 @@ function tabuada() {
     var num = document.getElementById('txtnum')
     var n1 = Number(num.value)
     var res = document.getElementById('res')
-
-    for (let i = 1; i <= 10; i++) {
+    var limite = 10
+    for (let i = 1; i <= limite; i++) {
         let m = n1 * i
-        res.innerHTML += `${n1} x ${i} = ${m}<br>`
+        if (i == 1) {
+            res.innerHTML = `${n1} x ${i} = <strong>${m}</strong><br>`
+        } else {
+            res.innerHTML += `${n1} x ${i} = <strong>${m}</strong><br>`
+        }
         
     }
 }
