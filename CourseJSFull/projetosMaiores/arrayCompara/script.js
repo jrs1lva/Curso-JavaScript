792075411668361cb0d8fc57ee6a17dab4830b45
Adailton Júnior <adailtonjunior0604@gmail.com>
2025-07-10 17:08:50 -0300

    Finalizando...

--- CourseJSFull/projetosMaiores/arrayCompara/script.js
+++ CourseJSFull/projetosMaiores/arrayCompara/script.js
@@ -12,7 +12,7 @@ function isNumero(n) {
 }
 
 function inLista(n,l) {
-    if(l.indexOf(Number(n)) != -1) {
+    if(l.indexOf(Number(n)) != -1) { //l.indexOf percorre a lista e o -1 indica que o número não foi encontrado na lista
         return true
     } else {
         return false
@@ -21,8 +21,52 @@ function inLista(n,l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0      
+
+        for (let pos in valores) {
+            soma+= valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+
+        /*for (let i = 1; i <= valores.length; i++) {
+            soma += valores[i]
+            if (valores[i] > maior) {
+                maior = valores[i]
+            } if (valores[i] < menor) {
+                menor = valores[i]
+            }
+        }*/ // deu merda
+        
+        media = soma / tot 
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número é ${maior}</p>`
+        res.innerHTML += `<p>O menor número é ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
     }
 }
